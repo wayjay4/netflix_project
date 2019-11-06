a5c05556d5a71c09bc5cb4a21acca5c5a37a9f84
Waylon Dixon <wdixon@waylondixon.com>
2019-11-06 14:56:24 -0700

    removed unnecessary files and coding for project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
+    <div>
+        <h1>Netflix Project</h1>
     </div>
   );
 }
