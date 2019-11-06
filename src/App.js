dd22b1a2d23e03cdf23fd36a39ba4695a3ce2250
Waylon Dixon <wdixon@waylondixon.com>
2019-11-06 15:04:51 -0700

    added Main component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import './css/App.css';
+import Main from './pages/index';
 
 function App() {
   return (
     <div>
-        <h1>Netflix Project</h1>
+      <Main />
     </div>
   );
 }
